08da94eef0b355340e011e2ad333ca5b74fc7e92
Jari Lammi <1969656+jarilammi@users.noreply.github.com>
2025-01-20 20:48:39 +0200

    Add basic status messages

--- Lutheran Radio/Lutheran Radio/ViewController.swift
+++ Lutheran Radio/Lutheran Radio/ViewController.swift
@@ -13,18 +13,72 @@ class ViewController: UIViewController {
     // Luodaan AVPlayer instanssi
     var player: AVPlayer?
 
+    // Status label
+    let statusLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Connecting…"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Setup UI
+        setupUI()
+
         // Aseta Icecastin streamin URL
-        let streamURL = URL(string: "https://livestream.lutheran.radio:8443/lutheranradio.mp3")!
+        guard let streamURL = URL(string: "https://livestream.lutheran.radio:8443/lutheranradio.mp3") else {
+            updateStatus("Error: Invalid stream URL")
+            return
+        }
 
         // Luo AVPlayer streamin URL:lle
         player = AVPlayer(url: streamURL)
 
-        // Aloita toisto
+        // Aloita toisto ja päivitä tila
+        updateStatus("Connecting…")
         player?.play()
+
+        // Tarkkaile toiston tilaa
+        observePlayerStatus()
     }
 
+    private func setupUI() {
+        view.backgroundColor = .white
+        view.addSubview(statusLabel)
 
+        // Center the status label in the view
+        NSLayoutConstraint.activate([
+            statusLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            statusLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+
+    private func updateStatus(_ text: String) {
+        DispatchQueue.main.async {
+            self.statusLabel.text = text
+        }
+    }
+
+    private func observePlayerStatus() {
+        // Tarkkaile playerin tilaa
+        player?.addObserver(self, forKeyPath: "timeControlStatus", options: [.new, .initial], context: nil)
+    }
+
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "timeControlStatus" {
+            if player?.timeControlStatus == .playing {
+                updateStatus("Playing")
+            } else if player?.timeControlStatus == .paused {
+                updateStatus("Paused")
+            }
+        }
+    }
+
+    deinit {
+        player?.removeObserver(self, forKeyPath: "timeControlStatus")
+    }
 }
-
